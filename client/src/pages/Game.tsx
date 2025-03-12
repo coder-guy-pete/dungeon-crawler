71a03d3fe0f00df93984db12b2c5f797d3ee81ef
Peter Hintze <hintze.peter@gmail.com>
2025-03-12 18:34:21 -0400

    basic project structure in place

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -1,11 +1,11 @@
 
 function Game() {
     return (
-      <div>
-        <h1>Dungeon Crawler</h1>
-        <p>The game will start here.</p>
-      </div>
+        <div>
+            <h1>Dungeon Crawler</h1>
+            <p>The game will start here.</p>
+        </div>
     );
-  }
-  
-  export default Game;
+}
+
+export default Game;
