362c5a004c1926e4816036dcc460347479df7eed
Peter Hintze <hintze.peter@gmail.com>
2025-03-12 18:40:17 -0400

    story segment model added

---
+++ server/src/models/StorySegent.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface Choice {
+    text: string;
+    nextSegmentId: mongoose.Types.ObjectId;
+    effects?: {
+        inventory?: { [key: string]: number };
+        stats?: { [key: string]: number };
+    };
+}
+
+interface StorySegmentDocument extends Document {
+    text: string;
+    choices: Choice[];
+    ending?: boolean;
+}
+
+const storySegmentSchema: Schema = new Schema({
+    text: { type: String, required: true },
+    choices: [
+        {
+        text: { type: String, required: true },
+        nextSegmentId: { type: Schema.Types.ObjectId, ref: 'StorySegment', required: true },
+        effects: {
+            inventory: { type: Schema.Types.Mixed },
+            stats: { type: Schema.Types.Mixed },
+        },
+        },
+    ],
+    ending: { type: Boolean, default: false },
+});
+
+export default mongoose.model<StorySegmentDocument>('StorySegment', storySegmentSchema);
