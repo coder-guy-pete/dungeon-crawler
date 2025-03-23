3810d788614293d2ededdbe3ffbc73390ed6acd3
Peter Hintze <hintze.peter@gmail.com>
2025-03-23 17:41:12 -0400

    Apollo Provider added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
+import { ApolloProvider } from '@apollo/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import client from './utils/apolloClient'
 import Game from './pages/Game'
 import './App.css'
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Game />} />
-      </Routes>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Game />} />
+        </Routes>
+      </Router>
+  </ApolloProvider>
   )
 }
 
