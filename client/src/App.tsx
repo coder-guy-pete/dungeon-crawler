a9d4ca0317ac7ff4ba0e1f58fb8d24826ba75f4d
Peter Hintze <hintze.peter@gmail.com>
2025-03-23 18:22:49 -0400

    page routing established

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
 import { ApolloProvider } from '@apollo/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import client from './utils/apolloClient'
+import Home from './pages/Home'
 import Game from './pages/Game'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import { useAuth, useAuthService } from './utils/auth'
 import './App.css'
 
 function App() {
+  const { loggedIn } = useAuth();
+  const authService = useAuthService();
+
+  const handleLogout = () => {
+    authService.logout();
+  };
 
   return (
     <ApolloProvider client={client}>
       <Router>
         <Routes>
-          <Route path="/" element={<Game />} />
+          <Route path="/" element={loggedIn ? <Game /> : <Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
+        {loggedIn && <button onClick={handleLogout}>Logout</button>}
       </Router>
   </ApolloProvider>
   )
