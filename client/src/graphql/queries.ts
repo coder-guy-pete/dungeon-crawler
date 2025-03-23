50de29e7b2a46ae0f1866697678e4b3b73d7699a
Peter Hintze <hintze.peter@gmail.com>
2025-03-23 17:20:16 -0400

    setup queries

---
+++ client/src/graphql/queries.ts
@@ -0,0 +1,37 @@
+import { gql } from '@apollo/client';
+
+export const GET_STORY_SEGMENT = gql`
+    query GetStorySegment($segmentId: Int!) {
+        getStorySegment(segmentId: $segmentId) {
+        id
+        segmentId
+        text
+        choices {
+            text
+            nextSegmentId
+            effects {
+            inventory
+            stats
+            }
+        }
+        ending
+        win
+        loss
+        }
+    }
+`;
+
+export const ME = gql`
+    query Me {
+        me {
+        id
+        username
+        email
+        inventory
+        stats
+        currentSegmentId
+        wins
+        losses
+        }
+    }
+`;
