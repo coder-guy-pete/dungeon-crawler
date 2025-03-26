8a3775b462e24a6e451212f16fc219a21bd27991
Peter Hintze <hintze.peter@gmail.com>
2025-03-26 15:35:02 -0400

    directory path fixed

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI || '';
+console.log('MONGODB_URI:', MONGODB_URI);
 
 const db = async (): Promise<typeof mongoose.connection> => {
     try {
