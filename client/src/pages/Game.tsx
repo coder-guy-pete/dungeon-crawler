98ca521ba20d331597872f10176590b689855735
Peter Hintze <hintze.peter@gmail.com>
2025-03-29 16:13:17 -0400

    game page structure in place

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -2,10 +2,9 @@ import { useState, useEffect } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_STORY_SEGMENT, ME } from '../graphql/queries';
 import { CHOOSE_PATH, RESET_GAME } from '../graphql/mutations';
-// import { useNavigate } from 'react-router-dom';
+import { Box, Card, Button, Flex, Heading, Text } from '@chakra-ui/react';
 
 function Game() {
-    // const navigate = useNavigate();
     const [segmentId, setSegmentId] = useState(0); 
     const { loading, error, data } = useQuery<{ getStorySegment: { text: string; choices: { text: string, nextSegmentId: number }[] } }>(GET_STORY_SEGMENT, { variables: { segmentId } });
     const { loading: meLoading, error: meError, data: meData } = useQuery(ME);
@@ -40,26 +39,42 @@ function Game() {
     };
 
     return (
-        <div>
-        <h1>Dungeon Crawler</h1>
-        {data?.getStorySegment && (
-            <div>
-            <p>{data.getStorySegment.text}</p>
-            {data.getStorySegment.choices.map((choice, index) => (
-                <button key={index} onClick={() => handleChoice(index)}>
-                {choice.text}
-                </button>
-            ))}
-            {meData?.me && (
-                <div>
-                <p>Inventory: {JSON.stringify(meData.me.inventory)}</p>
-                <p>Stats: {JSON.stringify(meData.me.stats)}</p>
-                <button onClick={handleResetGame}>Reset Game</button>
-                </div>
-            )}
-            </div>
-        )}
-        </div>
+        <Box background="blackAlpha.900">
+        <Box background="url('/imgs/prison_cell_closed.webp') no-repeat center center" w="100vw" h="100vh" display="flex" direction="row" justifyContent="center" alignItems="center">
+            <Card.Root size="lg" opacity={0.8}>
+                <Card.Body gap={4} backgroundColor="wheat" >
+                    <Heading size="2xl">Dungeon Crawler</Heading>
+                    {data?.getStorySegment && (
+                        <Box maxW="600px">
+                        <Text fontSize="md">{data.getStorySegment.text}</Text>
+                        <Flex direction="column" gap={4} justifyContent="space-between" mt={4}>
+                        {data.getStorySegment.choices.map((choice, index) => (
+                            <Button key={index} w="fit-content" onClick={() => handleChoice(index)}>
+                            {choice.text}
+                            </Button>
+                        ))}
+                        </Flex>
+                        {meData?.me && (
+                            <Box display="flex" flexDirection="column" justifyContent="center" gap={4} mt={4}>
+                                <Box display="flex" justifyContent="space-between" gap={4}>
+                                    <Flex direction="column" gap={2} mt={4} w="50%">
+                                        <Heading size="lg">Inventory:</Heading>
+                                        <Text>{JSON.stringify(meData.me.inventory)}</Text>
+                                    </Flex>
+                                    <Flex direction="column" gap={2} mt={4} w="50%">
+                                        <Heading size="lg">Stats:</Heading>
+                                        <Text>{JSON.stringify(meData.me.stats)}</Text>
+                                    </Flex>
+                                </Box>
+                                <Button w="fit-content" variant="surface" colorPalette="yellow" onClick={handleResetGame}>Reset Game</Button>
+                            </Box>
+                        )}
+                        </Box>
+                    )}
+                </Card.Body>
+            </Card.Root>
+        </Box>
+        </Box>
     );
 };
 
