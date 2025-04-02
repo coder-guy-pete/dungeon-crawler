fd9a1a683b12b290b416d54da798ffc81ca47c77
Peter Hintze <hintze.peter@gmail.com>
2025-04-01 22:39:31 -0400

    minor spelling errors

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -28,13 +28,13 @@ function Signup() {
         <Flex bg="blackAlpha.900" w="100vw" h="100vh" direction="row" justifyContent="center" alignItems="center">
             <Image src = "/imgs/torch.gif" alt="Animated Torch" h="200px" hideBelow="md" />
             <Box background="url('/imgs/scroll.png') no-repeat center center" backgroundSize="100% 100%" w="100%" minW="250px" maxW="500px" h="auto" minH="600px" display="flex" flexDirection="column" justifyContent="center" alignItems="center" opacity={0.8}>
-                <Heading size="4xl" pb={4}>Signup</Heading>
+                <Heading size="4xl" pb={4}>Sign Up</Heading>
                 <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '1rem', flex: 'start' }}>
                     <Input type="text" placeholder="Username" variant="subtle" backgroundColor="wheat" borderColor="black" value={username} onChange={(e) => setUsername(e.target.value)} />
                     <Input type="email" placeholder="Email" variant="subtle" backgroundColor="wheat" borderColor="black"  value={email} onChange={(e) => setEmail(e.target.value)} />
                     <Input type="password" placeholder="Password" variant="subtle" backgroundColor="wheat" borderColor="black"  value={password} onChange={(e) => setPassword(e.target.value)} />
                     <Flex justifyContent="space-between" gap={4} mt={4}>
-                        <Button type="submit">Signup</Button>
+                        <Button type="submit">Sign Up</Button>
                         <Button onClick={() => navigate('/')}>Home</Button>
                     </Flex>
                 </form>
